cddb23679e9ab348e817896b1fb22f06902b5046
Oleg283956 <oleg.m.1409@mail.ru>
2024-09-09 14:36:25 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -4,7 +4,7 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self,new_floor):
-        if isinstance(self, House) and isinstance(new_floor,int):
+        if isinstance(new_floor,int):
             if new_floor <= self.number_of_floors:
                  for i in range(1,new_floor+1):
                      print(i)

493b00468f3986e8fdfe6177aba05924132c1b1c
Oleg283956 <oleg.m.1409@mail.ru>
2024-10-06 07:29:14 +0300

    Add files via upload

--- module_9_7.py
+++ module_9_7.py
@@ -2,10 +2,15 @@ def is_prime(func):
     def wrapper(*args,**kwargs):
         original_result = func(*args,**kwargs)
         resultTMP = 'Простое' +'\n'+str(original_result)
-        for i in range(2,original_result - 1):
-            if original_result%i == 0:
-                resultTMP = 'Составное' +'\n'+str(original_result)
-                break
+        if original_result > 2:
+            for i in range(2,original_result):
+                if original_result%i == 0:
+                    resultTMP = 'Составное' +'\n'+str(original_result)
+                    break
+        if original_result <= 2 and original_result>0:
+            resultTMP = 'Простое' + '\n' + str(original_result)
+        if original_result < 0:
+            resultTMP = 'Отрицательное' + '\n' + str(original_result)
         return resultTMP
     return wrapper
 
@@ -14,5 +19,5 @@ def sum_three(a,b,c):
     return a+b+c
 
 
-result = sum_three(2, 3, 6)
+result = sum_three(0, 0, -1)
 print(result)
